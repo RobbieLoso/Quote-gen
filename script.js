43efce6252ba2179b93d4f2b1464a154d47e4e1b
Roberto Aparicio <rca.kt02@gmail.com>
2023-04-03 20:11:41 -0400

    js

---
+++ script.js
@@ -0,0 +1,13 @@
+const request = new XMLHttpRequest()
+request.open(GET /random,  'https://api.quotable.io', true)
+
+
+{
+    content: string
+    // The full name of the author
+    author: string
+    // The `slug` of the quote author
+    authorSlug: string
+    // The length of quote (number of characters)
+    length: number}
+    // An array of tag names for this quote
