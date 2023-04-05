7be54ab8f47bcd2fe3d26b3d5c32f00299423149
Stephen <112371588+stephen-legit@users.noreply.github.com>
2023-04-04 22:23:25 -0400

    API Boilerplate

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-const request = new XMLHttpRequest()
-request.open(GET /random,  'https://api.quotable.io', true)
+let temp1 = [];
 
-
-{
-    content: string
-    // The full name of the author
-    author: string
-    // The `slug` of the quote author
-    authorSlug: string
-    // The length of quote (number of characters)
-    length: number}
-    // An array of tag names for this quote
+function genQuote() {
+    fetch("https://api.quotable.io/random") 
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+    })
+}
