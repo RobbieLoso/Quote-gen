f247b3af517ce70e36c30ea76984e24ae266a5b3
Roberto Aparicio <rca.kt02@gmail.com>
2023-04-07 19:04:27 -0400

    added font color options

--- script.js
+++ script.js
@@ -14,19 +14,19 @@ function genQuote() {
 
 
 function orangeFont(){
-
+    document.getElementById('quote-font').style.color = '#F3A183'
 }
 function blueFont(){
-    
+    document.getElementById('quote-font').style.color = '#348ac7'
 }
 function grayFont(){
-    
+    document.getElementById('quote-font').style.color = '#859398'
 }
 function yellowFont(){
-    
+    document.getElementById('quote-font').style.color = '#ffeb9c'
 }
 function greenFont(){
-    
+    document.getElementById('quote-font').style.color = '#71b280'
 }
 
 
@@ -34,7 +34,7 @@ function greenFont(){
 
 
 function orangeBackground(){
-    document.getElementsByClassName('quote-card').style.background-Image(linear-gradient(to right, #f8d776 ,#f3a085))
+    document.getElementById('orange-background').style.backgroundColor='red'
 }
 function blueBackground(){
     
